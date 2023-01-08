9d908cdb5eecd408a1cf40ec271956472e29060a
UvejsGj <shiq.une1@gmail.com>
2023-01-08 13:17:37 +0100

    Added GitHub Logo to Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,28 +1,31 @@
 import "./Footer.css";
-import { BsInstagram } from "react-icons/bs";
 import { BsFacebook } from "react-icons/bs";
-import { BsTwitter } from "react-icons/bs"
+import { BsTwitter } from "react-icons/bs";
+import { SiGmail } from "react-icons/si";
+import { BsGithub } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <div>
       <footer>
         <div>
-      <a className="instalogo" href="https://www.instagram.com/uvejs_gjelaj/">
-          <BsInstagram />
-        </a>
-        <a
-          className="facebooklogo"
-          href="https://www.facebook.com/uvejs.gjelaj/"
-        >
-          <BsFacebook />
-        </a>
-        <a className="twitterlogo" href="https://twitter.com/uvejsgjelaj03">
-          <BsTwitter />
-        </a>
+          <a className="githublogo" href="https://github.com/UvejsGj">
+            <BsGithub />
+          </a>
+          <a
+            className="facebooklogo"
+            href="https://www.facebook.com/uvejs.gjelaj/"
+          >
+            <BsFacebook />
+          </a>
+          <a className="twitterlogo" href="https://twitter.com/uvejsgjelaj03">
+            <BsTwitter />
+          </a>
         </div>
-        <a className="name">© Uvejs Gjelaj</a>{" "}
-        
+        <a className="name">
+          © Uvejs Gjelaj <br /> <SiGmail className="gmail-logo" />{" "}
+          uvejsgjelaj03@gmail.com{" "}
+        </a>{" "}
       </footer>
     </div>
   );
