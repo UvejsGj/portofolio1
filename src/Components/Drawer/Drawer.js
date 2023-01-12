8fe510f69553a71ed5c9699a2dd8601c9a9390e9
UvejsGj <shiq.une1@gmail.com>
2023-01-12 21:26:21 +0100

    Commit

--- src/Components/Drawer/Drawer.js
+++ src/Components/Drawer/Drawer.js
@@ -1,18 +1,20 @@
-import "./Drawer.css"
+import "./Drawer.css";
 
 const Drawer = () => {
-    return(
-        <div className="drawer">
-            <div className="container">
-                <ul className="drawer-items">
-                    <li> Home </li>
-                    <li> About </li>
-                    <li> Work </li>
-                    <li> Contact me </li>
-                </ul>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="drawer">
+      <div className="container">
+        <ul className="drawer-items">
+          <Link>
+            <li> Home </li>
+            <li> About </li>
+            <li> Work </li>
+            <li> Contact me </li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Drawer;
---
+++ src/MenuPages/AboutPage/aboutpage.js
@@ -0,0 +1,11 @@
+import "./aboutpage.css";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1> I am Uvejs </h1>
+    </div>
+  );
+};
+
+export default AboutPage;
