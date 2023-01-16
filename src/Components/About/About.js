447e39da5f6cde8071d106b29253432292f0031e
UvejsGj <shiq.une1@gmail.com>
2023-01-16 20:52:48 +0100

    Commit

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,5 +1,6 @@
 import "./About.css";
 import aboutmepic from "../../Assets/images/aboutme1.png";
+import Button from "../../Components/Button/Button"
 
 const About = () => {
   return (
@@ -7,11 +8,13 @@ const About = () => {
       <div className="aboutmediv">
         <img className="aboutmeimg" src={aboutmepic} />
       </div>
-      <div>
+      <div className="aboutmepb">
         <p className="aboutmep">
           Hi, I am Uvejs from Peja. I'm learning React.js <br /> I also know
           HTML & CSS.
         </p>
+        
+        <Button/>
       </div>
     </div>
   );
--- src/MenuPages/HomePage/homepage.js
+++ src/MenuPages/HomePage/homepage.js
@@ -1,16 +1,14 @@
 import Navbar from "../../Components/Navbar/Navbar"
 import About from "../../Components/About/About"
 import Footer from "../../Components/Footer/Footer"
-import Button from "../../Components/Button/Button"
+
 
 function HomePage() {
     return(
-        <div>
-        <h1> WELCOME </h1>
+      <div>
       <Navbar />
       <About />
       <Footer />
-      <Button />
       </div>
     )
 }
