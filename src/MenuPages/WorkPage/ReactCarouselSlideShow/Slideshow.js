b6640c65fd7acd6388c4ea6685c7cd7dd7d9a6c5
UvejsGj <shiq.une1@gmail.com>
2023-01-17 15:21:54 +0100

    Project Added

--- src/MenuPages/WorkPage/ReactCarouselSlideShow/Slideshow.js
+++ src/MenuPages/WorkPage/ReactCarouselSlideShow/Slideshow.js
@@ -1,29 +1,33 @@
 import { Carousel } from "react-responsive-carousel";
+import "./Slideshow.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import FaceProject from "../../../Assets/images/faceproject.png";
 import BBCProject from "../../../Assets/images/bccproject.png";
+import AskTheX from "../../../Assets/images/askthex.png";
 
 const Slideshow = () => {
-    return(
+  return (
+    <div className="carouselslideshow">
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={true}
+        showIndicators={true}
+        showThumbs={true}
+        interval={5000}
+      >
         <div>
-            <Carousel
-            autoPlay
-            infiniteLoop
-            showStatus={true}
-            showIndicators={true}
-            showThumbs={true}
-            interval={5000}
-            >
-                <div>
-                    <img loading="lazy" src={FaceProject}/>
-                </div>
-                 <div>
-                    <img loading="lazy" src={BBCProject}/>
-                </div> 
-                
-            </Carousel>
+          <img loading="lazy" src={FaceProject} />
         </div>
-    )
-}
+        <div>
+          <img loading="lazy" src={BBCProject} />
+        </div>
+        <div>
+          <img loading="lazy" src={AskTheX} />
+        </div>
+      </Carousel>
+    </div>
+  );
+};
 
 export default Slideshow;
