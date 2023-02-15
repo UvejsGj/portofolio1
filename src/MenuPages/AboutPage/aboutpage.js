2a0e2cf4fc854d5c325c28aabb09ffbcb7e7b190
UvejsGj <shiq.une1@gmail.com>
2023-02-15 19:30:58 +0100

    Commit

--- src/MenuPages/AboutPage/aboutpage.js
+++ src/MenuPages/AboutPage/aboutpage.js
@@ -4,6 +4,7 @@ import Navbar from "../../Components/Navbar/Navbar";
 import { AiFillHtml5 } from "react-icons/ai";
 import { FaCss3Alt } from "react-icons/fa";
 import { DiJavascript } from "react-icons/di";
+import { BsGithub } from "react-icons/bs"
 
 // https://www.geeksforgeeks.org/how-to-animate-a-progress-bar-in-bootstrap/
 
@@ -14,31 +15,26 @@ const AboutPage = () => {
       <Navbar />
 
       <div className="skilldiv">
-        My Skills:
-        <br></br>
-        HTML <AiFillHtml5 />
-        <br></br>
-        CSS <FaCss3Alt />
-        <br></br>
-        JS <DiJavascript />
+      <h1>My Skills:</h1>
+      <div className="icons" >
+        <AiFillHtml5 className="htmlicon" />
+        <FaCss3Alt className="cssicon" />
+        <DiJavascript className="javaicon"/>
+        <BsGithub className="giticon" />
+        </div> 
       </div>
 
       <div className="certificate">
         <h1 className="certificates">
           My certificates:
           </h1>
-          <p className="certificates">
-          <br></br>
-          1. Completed Course of Web and Mobile Game Development with STENCYL.
-          <br></br>
-          2. Completed Course of JavaScript Fundamentals - Beginner
-          <br></br>
-          3. Completed Course of Web Development - Beginner
-          <br></br>
-          4. Completed Course of Web Development
-          <br></br>
-          5. Completed Course of React JS Basics
-          </p>
+          <ol className="certificatesp">
+          <li>Completed Course of Web and Mobile Game Development with STENCYL</li>
+          <li>Completed Course of JavaScript Fundamentals - Beginner </li>
+          <li> Completed Course of Web Development - Beginner </li>
+          <li> Completed Course of Web Development </li>
+          <li>Completed Course of React JS Basics </li>
+          </ol>
       </div>
     </div>
   );
